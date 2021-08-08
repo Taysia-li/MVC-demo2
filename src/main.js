fa7cab296e8517ebddbbe9a34acab255cf0ada14
Taysia-li <87682005+Taysia-li@users.noreply.github.com>
2021-08-08 22:33:34 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import './global';
 
 import x from './app1.js';
 
-import './app2.js';
+import y from './app2.js';
 
 import './app3.js';
 
 import './app4.js';
 
 x.init('#app1');
+y.init('#app2);
